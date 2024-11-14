e2992e2f5a52fa4227984396da64a6a175f51fec
Bartosz <bartosz@gerlowski.pl>
2024-11-14 15:59:53 +0100

    Remove TOKEN validation from entrypoint.sh and simplify cron configuration
    - Removed the TOKEN validation logic from entrypoint.sh.
    - Both cron jobs are now added unconditionally, assuming TOKEN is already set.
    - Cron jobs for both API endpoints (network range and investment plans) are added.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-# Sprawdzenie, czy TOKEN jest ustawiony
-if [ -z "$TOKEN" ]; then
-  echo "ERROR: TOKEN API is not set. Please set the TOKEN in the .env file."
-  exit 1
-fi
+# Konfiguracja zadania cron z dwoma wpisami
+echo "0 12 * * * curl --location --request PUT 'https://internet.gov.pl/api/statement/' --header 'Accept: application/json' --header 'Content-Type: application/json' --header 'Authorization: Token $TOKEN' --data '{\"are_up_to_date\": true}' >/dev/null 2>&1" > /etc/crontabs/root
+echo "0 12 * * * curl --location --request PUT 'https://internet.gov.pl/api/statement/investment_plans/' --header 'Accept: application/json' --header 'Content-Type: application/json' --header 'Authorization: Token $TOKEN' --data '{\"are_up_to_date\": true}' >/dev/null 2>&1" >> /etc/crontabs/root
 
-# Dodanie zadań do crontaba
-echo "0 12 * * * curl --location --request PUT 'https://internet.gov.pl/api/statement/' --header 'Accept: application/json' --header 'Content-Type: application/json' --header 'Authorization: Token $TOKEN' --data '{\"are_up_to_date\": true}' >/dev/null 2>&1" | crontab -
-echo "0 12 * * * curl --location --request PUT 'https://internet.gov.pl/api/statement/investment_plans/' --header 'Accept: application/json' --header 'Content-Type: application/json' --header 'Authorization: Token $TOKEN' --data '{\"are_up_to_date\": true}' >/dev/null 2>&1" | crontab -
-
-# Uruchom cron w trybie pierwszoplanowym
+# Uruchomienie crond
 crond -f
